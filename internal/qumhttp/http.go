bd790dc2b2194272c68b470fb1ce2719fe23a9b8
Pavel Parshin <p.parshin@city-mobil.ru>
2020-05-26 19:21:20 +0300

    Added endpoint to dump app version and build date

--- internal/qumhttp/http.go
+++ internal/qumhttp/http.go
@@ -1,6 +1,9 @@
 package qumhttp
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 func HealthHandler() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -11,5 +14,28 @@ func HealthHandler() http.Handler {
 
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
+	})
+}
+
+func AboutHandler(version, buildDate string) http.Handler {
+	about := struct {
+		Version string `json:"version"`
+		Build   string `json:"build"`
+	}{
+		Version: version,
+		Build:   buildDate,
+	}
+
+	aboutStr, _ := json.Marshal(about)
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write(aboutStr)
 	})
 }
